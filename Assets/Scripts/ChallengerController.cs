b05f6f5d323aba763dfc399fcd348c71b783482c
Franquelie Masanque <franquelie@gmail.com>
2025-06-14 18:31:48 +0800

    Added scripts for npc dialog

---
+++ Assets/Scripts/ChallengerController.cs
@@ -0,0 +1,12 @@
+// using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengerControleer : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        Debug.Log("You will start a battle!");
+    }
+}
